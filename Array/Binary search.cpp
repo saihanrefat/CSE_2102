d6c5d18d0d84c70a17e008d5b280502518c28754
SaihanulRefat <saihanulislam23@gmail.com>
2025-04-28 20:44:38 +0600

    Update Binary search.cpp

--- Array/Binary search.cpp
+++ Array/Binary search.cpp
@@ -5,22 +5,26 @@ int main()
     int arr[12]={11,22,30,40,44,55,60,66,77,80,88,99};
     int left,right,middle,item;
     left=0;
-    right=11;
-    item=300;
+    right=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Enter searching item: ";
+    cin>>item;
     while(left<=right)
     {
-         middle=(left+right)/2;
+        middle=(left+right)/2;
         if(arr[middle]==item)
         {
             cout<<middle;
             return 0;
-        }else if(arr[middle]>item)
-        {
-            right=middle-1;
-        }else
+        }
+        else if(arr[middle]<item)
         {
             left=middle+1;
         }
+        else
+        {
+            right=middle-1;
+        }
     }
-    cout<<"Item not found"<<endl;
+    cout<<"Item Not found"<<endl;
 }
+
