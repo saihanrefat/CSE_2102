fd0d73c6b643613173353fb232aecedc741a2b44
SaihanulRefat <saihanulislam23@gmail.com>
2025-05-08 23:17:25 +0600

    Add files via upload

---
+++ LinkedList/Delete a node at the End.cpp
@@ -0,0 +1,75 @@
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *CreatLinkedList(int arr[],int size);
+void DeleteNode(struct Node *head);
+void print(struct Node *head);
+int main()
+{
+    int n;
+    cout<<"Enter array size:";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array element:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    struct Node *head;
+    head = CreatLinkedList(arr,n);
+    DeleteNode(head);
+}
+struct Node *CreatLinkedList(int arr[],int size)
+{
+    struct Node *head=NULL,*current=NULL,*temp=NULL;
+    for(int i=0;i<size;i++)
+    {
+        temp=(struct Node *)malloc(sizeof(struct Node));
+        temp->data=arr[i];
+        temp->next=NULL;
+        if(head==NULL)
+        {
+            head=temp;
+            current=temp;
+        }
+        else
+        {
+            current->next=temp;
+            current=current->next;
+        }
+
+    }
+    return head;
+};
+void print(struct Node *head)
+{
+   while(head !=NULL)
+    {
+        cout<<head->data<<" ->";
+        head=head->next;
+    }
+    cout<<"NULL";
+}
+void DeleteNode(struct Node *head)
+{
+    struct Node *newhead= head;
+    struct Node *temp;
+    if(newhead->next==NULL || newhead ==NULL)
+    {
+        return;
+    }
+    while(newhead->next !=NULL)
+    {
+        temp=newhead;
+        newhead=newhead->next;
+    }
+  temp->next=NULL;
+    print(head);
+}
+
